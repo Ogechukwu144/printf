6e4a766e9938edcb1915e0251ce5a76052367407
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-11-22 20:55:36 -0800

    write files

--- write_handlers.c
+++ write_handlers.c
@@ -101,7 +101,7 @@ int write_num(int ind, char buffer[],
 	int i, padd_start = 1;
 
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
-		return (0); /* printf(".0d", 0)  no char is printed */
+		return (0);
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 		buffer[ind] = padd = ' '; /* width is displayed with padding ' ' */
 	if (prec > 0 && prec < length)
@@ -186,12 +186,12 @@ int write_unsgnd(int is_negative, int ind,
 
 		buffer[i] = '\0';
 
-		/* Assign extra char to left of buffer [buffer > padd] */
+		/* Assign extra char to left of buffer [buffer>padd] */
 		if (flags & F_MINUS)
 		{
 			return (write(1, &buffer[ind], length) + write(1, &buffer[0], i));
-		{
-		else /* Assign extra char to left of padding [padd > buffer] */
+		}
+		else /* Assign extra char to left of padding [padd>buffer] */
 		{
 			return (write(1, &buffer[0], i) + write(1, &buffer[ind], length));
 		}
@@ -225,6 +225,14 @@ int write_pointer(char buffer[], int ind, int length,
 			buffer[i] = padd;
 		buffer[i] = '\0';
 		if (flags & F_MINUS && padd == ' ') /* Assign extra char to left of buffer */
+		{
+			buffer[--ind] = 'x';
+			buffer[--ind] = '0';
+			if (extra_c)
+				buffer[--ind] = extra_c;
+			return (write(1, &buffer[3], i - 3) + write(1, &buffer[ind], length));
+		}
+		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buffer */
 		{
 			buffer[--ind] = 'x';
 			buffer[--ind] = '0';
